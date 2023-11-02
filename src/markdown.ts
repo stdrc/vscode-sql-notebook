066715c31ae326235d80d3891c143c0738c8e3f3
Richard Chien <stdrc@outlook.com>
2023-11-02 17:23:31 +0800

    feat: display query outputs in monospace font
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- src/markdown.ts
+++ src/markdown.ts
@@ -13,7 +13,31 @@ export function resultToMarkdownTable(result: TabularResult): string {
       Object.fromEntries(Object.entries(result).map((pair) => [pair[0], '...']))
     );
   }
-  return `${markdownHeader(result[0])}\n${result.map(markdownRow).join('\n')}`;
+
+  return `
+  <div>
+  <style scoped>
+    table {
+      font-family: ${getFontFamily()}
+    }
+    table tbody tr:not(:first-child) td {
+      border-top: none;
+      padding-top: 3px;
+    }
+    table tbody tr:not(:last-child) td {
+      border-bottom: none;
+      padding-bottom: 3px;
+    }
+  </style>
+  <table>
+    <thead>
+      ${htmlHeaderRow(result[0])}
+    </thead>
+    <tbody>
+      ${result.map(htmlBodyRow).join('\n')}
+    </tbody>
+  </table>
+  </div>`;
 }
 
 function getMaxRows(): number {
@@ -24,7 +48,26 @@ function getMaxRows(): number {
   return maxRows ?? fallbackMaxRows;
 }
 
-function serializeCell(a: any): any {
+function htmlHeaderRow(obj: Row): string {
+  const content = Object.keys(obj)
+    .map(stringifyCell)
+    .map(escapeCell)
+    .map((colName) => `<th>${colName}</th>`)
+    .join();
+  return `<tr>${content}</tr>`;
+}
+
+function htmlBodyRow(row: Row): string {
+  const content = Object.entries(row)
+    .map((pair) => pair[1])
+    .map(stringifyCell)
+    .map(escapeCell)
+    .map((val) => `<td>${val}</td>`)
+    .join();
+  return `<tr>${content}</tr>`
+}
+
+function stringifyCell(a: any): string {
   try {
     // serialize buffers as hex strings
     if (Buffer.isBuffer(a)) {
@@ -35,26 +78,25 @@ function serializeCell(a: any): any {
       return JSON.stringify(a);
     }
     if (typeof a === 'string') {
-      return a.replace(/\n/g, '\\n').replace(/\r/g, '\\r');
+      return a.replace(/[\n\r]+/g, ' ');
     }
-    return a;
   } catch {
-    return a;
   }
+  return `${a}`;
 }
 
-function markdownRow(row: Row): string {
-  const middle = Object.entries(row)
-    .map((pair) => pair[1])
-    .map(serializeCell)
-    .join(' | ');
-  return `| ${middle} |`;
+function escapeCell(s: string): string {
+  return s
+    .replace(/&/g, '&amp;')
+    .replace(/"/g, '&quot;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/\\/g, '&apos;')
+    .replace(/ /g, '&nbsp;');
 }
 
-function markdownHeader(obj: Row): string {
-  const keys = Object.keys(obj).join(' | ');
-  const divider = Object.keys(obj)
-    .map(() => '--')
-    .join(' | ');
-  return `| ${keys} |\n| ${divider} |`;
+function getFontFamily(): string {
+  const editorConfig = vscode.workspace.getConfiguration('editor');
+  const fontFamily = editorConfig.get('fontFamily');
+  return typeof fontFamily === 'string' ? fontFamily : 'monospace';
 }
